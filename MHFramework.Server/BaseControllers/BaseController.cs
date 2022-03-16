68a746dd3c78d0f45433c068724433c548612d1d
Mohamed Hasabalah <68168970+mhasabalah@users.noreply.github.com>
2022-03-16 14:08:05 +0200

    Add BaseController to server.cs

---
+++ MHFramework.Server/BaseControllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿namespace MHFramework.Server;
+
+[Route("api/[controller]")]
+[ApiController]
+public abstract class BaseController<TEntity> : ControllerBase
+    where TEntity : BaseEntity
+{
+    private readonly IBaseUnitOfWork<TEntity> _unitOfWork;
+    public BaseController(IBaseUnitOfWork<TEntity> unitOfWork) => _unitOfWork = unitOfWork;
+
+    [HttpPost]
+    public virtual async Task Post(TEntity entity) => await _unitOfWork.Create(entity);
+
+    [HttpGet]
+    public virtual async Task<IEnumerable<TEntity>> Get() => await _unitOfWork.Read();
+    [HttpGet("{id}")]
+    public virtual async Task<TEntity> Get([FromRoute] Guid id) => await _unitOfWork.Read(id);
+
+    [HttpPut]
+    public virtual async Task Put(TEntity entity) => await _unitOfWork.Update(entity);
+
+    [HttpDelete("{id}")]
+    public virtual async Task Delete(Guid id) => await _unitOfWork.Delete(id);
+}
---
+++ MHFramework.Server/BaseControllers/BaseSettingsController.cs
@@ -0,0 +1,14 @@
+﻿namespace MHFramework.Server;
+[Route("api/[controller]")]
+[ApiController]
+public abstract class BaseSettingsController<TEntity> : BaseController<TEntity>
+    where TEntity : BaseSettingsEntity
+{
+    private readonly IBaseSettingsUnitOfWork<TEntity> _baseSettingsUnitOfWork;
+    public BaseSettingsController(IBaseSettingsUnitOfWork<TEntity> unitOfWork) : base(unitOfWork) =>
+        _baseSettingsUnitOfWork = unitOfWork;
+
+
+    [HttpGet("Search/{searchText}")]
+    public virtual async Task<IEnumerable<TEntity>> Search([FromRoute] string searchText) => await _baseSettingsUnitOfWork.Search(searchText);
+}
