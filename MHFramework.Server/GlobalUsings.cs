6941c02c3bfabd361fd91547775a13a857fe6450
Mohamed Hasabalah <68168970+mhasabalah@users.noreply.github.com>
2022-03-16 15:26:55 +0200

    format GlobalUsings

--- MHFramework.Server/GlobalUsings.cs
+++ MHFramework.Server/GlobalUsings.cs
@@ -1,12 +1,11 @@
-﻿global using Microsoft.EntityFrameworkCore.Storage;
-global using Microsoft.EntityFrameworkCore;
+﻿global using MHFramework.shared;
+
 global using System.Reflection;
 
+global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Microsoft.AspNetCore.Mvc;
-
-
-global using MHFramework.shared;
-global using Microsoft.EntityFrameworkCore.ValueGeneration;
 global using Microsoft.Extensions.Configuration;
+global using Microsoft.EntityFrameworkCore.Storage;
+global using Microsoft.EntityFrameworkCore.ValueGeneration;
 global using Microsoft.Extensions.DependencyInjection;
--- MHFramework.Shared/GlobalUsings.cs
+++ MHFramework.Shared/GlobalUsings.cs
@@ -1,2 +1,3 @@
 ﻿global using FluentValidation;
+
 global using Microsoft.Extensions.Localization;
