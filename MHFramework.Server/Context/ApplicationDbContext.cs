560568244510172245a4840f95652dcb46b5e80f
Mohamed Hasabalah <68168970+mhasabalah@users.noreply.github.com>
2022-03-16 14:10:28 +0200

    add mainContext

---
+++ MHFramework.Server/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace MHFramework.Server;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    //protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //    => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+    }
+}
