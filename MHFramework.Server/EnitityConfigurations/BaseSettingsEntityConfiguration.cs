012b14aed6b21a6d2b2ce5336ffe876a3d802358
Mohamed Hasabalah <68168970+mhasabalah@users.noreply.github.com>
2022-03-16 14:10:54 +0200

    Add baseEntityConfigrations to server.cs

---
+++ MHFramework.Server/EnitityConfigurations/BaseSettingsEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace MHFramework.Server;
+public class BaseSettingsEntityConfiguration<TEntity> : BaseEntityConfiguration<TEntity>, IEntityTypeConfiguration<TEntity>
+    where TEntity : BaseSettingsEntity
+{
+    public virtual new void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        base.Configure(builder);
+
+        builder.HasIndex(e => e.Name).IsUnique();
+
+        builder.Property(e => e.Name).IsRequired();
+        builder.Property(e => e.Name).HasMaxLength(50);
+    }
+}
