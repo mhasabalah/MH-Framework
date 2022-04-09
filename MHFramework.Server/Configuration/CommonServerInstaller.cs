88a70b3b973505994537dfd55a5cb01d3bde43b9
Mohamed Hasabalah <68168970+mhasabalah@users.noreply.github.com>
2022-04-09 05:39:22 +0200

    Configration

---
+++ MHFramework.Server/Configuration/CommonServerInstaller.cs
@@ -0,0 +1,17 @@
+﻿namespace MHFramework.Server;
+public class CommonServerInstaller : IInstaller
+{
+    public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped(typeof(IBaseSettingsRepository<>), typeof(BaseSettingsRepository<>));
+        services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+        //services.AddScoped(typeof(IBaseSettingsUnitOfWork<,>), typeof(BaseSettingsUnitOfWork<,>));
+        services.AddScoped(typeof(IBaseUnitOfWork<,>), typeof(BaseUnitOfWork<,>));
+
+        //services.AddFluentValidation(options =>
+        //{
+        //    options.AutomaticValidationEnabled = true;
+        //    options.DisableDataAnnotationsValidation = true;
+        //});
+    }
+}
