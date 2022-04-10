2c3a5f53efc1ac8d71db173a41ef71dc936493cb
Mohamed Hasabalah <68168970+mhasabalah@users.noreply.github.com>
2022-04-10 22:11:20 +0200

    controller delete comment

--- MHFramework.Server/BaseControllers/BaseController.cs
+++ MHFramework.Server/BaseControllers/BaseController.cs
@@ -68,17 +68,4 @@ public abstract class BaseController<TEntity, TViewModel> : ControllerBase
         TViewModel viewModel = await _unitOfWork.Delete(id);
         return Ok(viewModel);
     }
-
-    //[HttpPost]
-    //public virtual async Task Post(TEntity entity) => await _unitOfWork.Create(entity);
-    //[HttpGet]
-    //public virtual async Task<IEnumerable<TEntity>> Get() => await _unitOfWork.Read();
-    //[HttpGet("{id}")]
-    //public virtual async Task<TEntity> Get([FromRoute] Guid id) => await _unitOfWork.Read(id);
-
-    //[HttpPut]
-    //public virtual async Task Put(TEntity entity) => await _unitOfWork.Update(entity);
-
-    //[HttpDelete("{id}")]
-    //public virtual async Task Delete(Guid id) => await _unitOfWork.Delete(id);
 }
