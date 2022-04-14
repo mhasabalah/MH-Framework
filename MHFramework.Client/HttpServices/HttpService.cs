3f5e632ba63b2b204624b800cb15f4e2fc0e8960
Mohamed Hasabalah <68168970+mhasabalah@users.noreply.github.com>
2022-04-14 19:59:07 +0200

    Add HttpService to Client

---
+++ MHFramework.Client/HttpServices/HttpService.cs
@@ -0,0 +1,152 @@
+﻿namespace MHFramework.Client;
+public class HttpService<TViewModel> : IHttpService<TViewModel>
+    where TViewModel : BaseViewModel
+{
+    private readonly HttpClient _httpClient;
+    private readonly IToastService _toastService;
+
+    public HttpService(HttpClient httpClient, IToastService toastService)
+    {
+        _httpClient = httpClient;
+        _toastService = toastService;
+    }
+
+    public virtual async Task<IEnumerable<TViewModel>> GetAsync(string url)
+    {
+        IEnumerable<TViewModel> viewModels = new List<TViewModel>();
+        try
+        {
+            HttpResponseMessage result = await _httpClient.GetAsync(url);
+            string content = await result.Content.ReadAsStringAsync();
+
+            if (!result.IsSuccessStatusCode)
+            {
+                // Can log here
+                Console.WriteLine(content);
+                throw new Exception(content);
+            }
+
+            Console.WriteLine("Get successfully");
+            viewModels = JsonConvert.DeserializeObject<IEnumerable<TViewModel>>(content);
+            return viewModels;
+        }
+        catch (Exception exception)
+        {
+            // Can log here
+            Console.WriteLine(exception.Message);
+            _toastService.ShowToast(ToastLevel.Error, exception.Message);
+        }
+        return viewModels;
+    }
+    public virtual async Task<TViewModel> GetByIdAsync(string url)
+    {
+        TViewModel viewModel = null;
+        try
+        {
+            HttpResponseMessage result = await _httpClient.GetAsync(url);
+
+            string resultContent = await result.Content.ReadAsStringAsync();
+            if (!result.IsSuccessStatusCode)
+            {
+                // Can log or show Toaster with message that contain that error.
+                Console.WriteLine(resultContent);
+                throw new Exception(resultContent);
+            }
+
+            Console.WriteLine("GetById successfully");
+            viewModel = JsonConvert.DeserializeObject<TViewModel>(resultContent);
+            return viewModel;
+        }
+        catch (Exception exception)
+        {
+            // Can log here
+            Console.WriteLine(exception.Message);
+            _toastService.ShowError(exception.Message);
+        }
+        return viewModel;
+    }
+    public virtual async Task<TViewModel> PostAsync(string url, TViewModel viewModel)
+    {
+        try
+        {
+            var content = JsonConvert.SerializeObject(viewModel);
+            var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage result = await _httpClient.PostAsync(url, bodyContent);
+
+            string resultContent = await result.Content.ReadAsStringAsync();
+            if (!result.IsSuccessStatusCode)
+            {
+                // Can log or show Toaster with message that contain that error.
+                Console.WriteLine(resultContent);
+                throw new Exception(resultContent);
+            }
+
+            Console.WriteLine("Post successfully");
+            viewModel = JsonConvert.DeserializeObject<TViewModel>(resultContent);
+            return viewModel;
+        }
+        catch (Exception exception)
+        {
+            // Can log here
+            Console.WriteLine(exception.Message);
+            _toastService.ShowError(exception.Message);
+        }
+        return viewModel;
+    }
+    public virtual async Task<TViewModel> PutAsync(string url, TViewModel viewModel)
+    {
+        try
+        {
+            var content = JsonConvert.SerializeObject(viewModel);
+            var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage result = await _httpClient.PutAsync(url, bodyContent);
+
+            string resultContent = await result.Content.ReadAsStringAsync();
+            if (!result.IsSuccessStatusCode)
+            {
+                // Can log here
+                Console.WriteLine(resultContent);
+                throw new Exception(resultContent);
+            }
+
+            Console.WriteLine("Put successfully");
+            viewModel = JsonConvert.DeserializeObject<TViewModel>(resultContent);
+            return viewModel;
+        }
+        catch (Exception exception)
+        {
+            // Can log here
+            Console.WriteLine(exception.Message);
+            _toastService.ShowError(exception.Message);
+        }
+        return viewModel;
+    }
+    public virtual async Task<TViewModel> DeleteAsync(string url)
+    {
+        TViewModel viewModel = null;
+        try
+        {
+            HttpResponseMessage result = await _httpClient.DeleteAsync(url);
+
+            string resultContent = await result.Content.ReadAsStringAsync();
+            if (!result.IsSuccessStatusCode)
+            {
+                // Can log or show Toaster with message that contain that error.
+                Console.WriteLine(resultContent);
+            }
+
+            Console.WriteLine("Delete successfully");
+            viewModel = JsonConvert.DeserializeObject<TViewModel>(resultContent);
+            return viewModel;
+        }
+        catch (Exception exception)
+        {
+            // Can log here
+            Console.WriteLine(exception.Message);
+            _toastService.ShowError(exception.Message);
+        }
+        return viewModel;
+    }
+}
---
+++ MHFramework.Client/HttpServices/IHttpService.cs
@@ -0,0 +1,10 @@
+﻿namespace MHFramework.Client;
+
+public interface IHttpService<TViewModel> where TViewModel : BaseViewModel
+{
+    Task<TViewModel> DeleteAsync(string url);
+    Task<IEnumerable<TViewModel>> GetAsync(string url);
+    Task<TViewModel> GetByIdAsync(string url);
+    Task<TViewModel> PostAsync(string url, TViewModel viewModel);
+    Task<TViewModel> PutAsync(string url, TViewModel viewModel);
+}
