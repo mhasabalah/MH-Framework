7b1939a289014dab21d15a7789e07e1e8fdabcb0
Mohamed Hasabalah <68168970+mhasabalah@users.noreply.github.com>
2022-04-14 12:02:43 +0200

    Fix Commom Configration Service

--- MHFramework.Server/Configuration/CommonServerInstaller.cs
+++ MHFramework.Server/Configuration/CommonServerInstaller.cs
@@ -3,10 +3,10 @@ public class CommonServerInstaller : IInstaller
 {
     public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
-        services.AddScoped(typeof(IBaseSettingsRepository<>), typeof(BaseSettingsRepository<>));
         services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
-        services.AddScoped(typeof(IBaseSettingsUnitOfWork<,>), typeof(BaseSettingsUnitOfWork<,>));
+        services.AddScoped(typeof(IBaseSettingsRepository<>), typeof(BaseSettingsRepository<>));
         services.AddScoped(typeof(IBaseUnitOfWork<,>), typeof(BaseUnitOfWork<,>));
+        services.AddScoped(typeof(IBaseSettingsUnitOfWork<,>), typeof(BaseSettingsUnitOfWork<,>));
 
         services.AddFluentValidation(options =>
         {
