e30529ab29c511e2257fe3d258d602087a1f89b3
Mohamed Hasabalah <68168970+mhasabalah@users.noreply.github.com>
2022-04-30 00:47:21 +0200

    fix entity configration

--- MHFramework.Server/EnitityConfigurations/BaseSettingsEntityConfiguration.cs
+++ MHFramework.Server/EnitityConfigurations/BaseSettingsEntityConfiguration.cs
@@ -2,13 +2,11 @@
 public class BaseSettingsEntityConfiguration<TEntity> : BaseEntityConfiguration<TEntity>, IEntityTypeConfiguration<TEntity>
     where TEntity : BaseSettingsEntity
 {
-    public virtual new void Configure(EntityTypeBuilder<TEntity> builder)
+    public new virtual void Configure(EntityTypeBuilder<TEntity> builder)
     {
         base.Configure(builder);
 
         builder.HasIndex(e => e.Name).IsUnique();
-
-        builder.Property(e => e.Name).IsRequired();
-        builder.Property(e => e.Name).HasMaxLength(50);
+        builder.Property(e => e.Name).IsRequired().HasMaxLength(500); ;
     }
 }
